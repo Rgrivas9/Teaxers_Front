a2f5fb90c8e1aab9d5d2d258ac8a137cb920d4d0
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-04-11 15:37:56 +0200

    palette

--- src/styles/Palette.tsx
+++ src/styles/Palette.tsx
@@ -1,15 +1,13 @@
 const Palette = {
-  header: {
-    main: '#8B08F7',
-    blue: 'rgb(0,151,255)',
-    blue_gradient:
-      'linear-gradient(180deg, rgba(0,151,255,1) 0%, rgba(0,105,255,1) 100%)',
-    grey: '#414141',
-  },
+  primary: '#8B08F7',
+  secondary: '#404ec6',
+  gradient:
+    'linear-gradient(180deg, rgba(139,8,247,1) 0%, rgba(64,78,198,1) 100%);',
+  grey: '#414141',
   font: { _white: '#fefffb', _grey: '#2d2d2d' },
   main: '#ffffff',
   footer: '#1e1e1e',
   anchor_background: '#000000',
-  transparent: 'transparent'
+  transparent: 'transparent',
 }
 export default Palette
